96f5785c6fda7094d480b9140ea3846fcd85ee6c
Corey Wang <corefire2004@gmail.com>
2022-04-14 14:28:09 -0400

    simplify and clear screen

--- a1z26.py
+++ a1z26.py
@@ -1,27 +1,17 @@
 from random import *
 import time
-letters = "abcdefghijklmnopqrstuvwxyz"
-letters_list=list(letters)
-enum_list=list(enumerate(letters_list,1))
-
-letter_first = [(l[1],l[0]) for l in enum_list]
-enum_list.extend(letter_first)
+enum_list=list(enumerate(list("abcdefghijklmnopqrstuvwxyz"),1))
+enum_list.extend([(l[1],l[0]) for l in enum_list])
+qs_answered, correct, time_limit = 0,0,30
 start_time=time.time()
-qs_answered=0
-correct=0
-time_limit=6
 while time.time()-start_time<time_limit:
     c=choice(enum_list)
-    print(c[0])
-    i=input()
-    qs_answered+=1
-    if i==str(c[1]):
-        print("correct")
-        correct+=1
-    else:
-        print("incorrect")
+    print("\n"*20)
+    print(str(c[0])+"\n")
     time_diff=time.time()-start_time
-    print(f'You take {time_diff/qs_answered} seconds on average')
-
+    print(f'You have {round(time_limit-time_diff,3)} seconds left')
+    qs_answered+=1
+    if input()==str(c[1]): correct+=1
+    
 print(f'After {time_limit} seconds, your average answers per second is ' 
     f'{round(time_diff/qs_answered,3)}, and you answered {correct}/{qs_answered} questions correctly.')
