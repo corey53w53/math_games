76e254ad2315a8ca56bfa7771c0cd5528a957575
Corey Wang <corefire2004@gmail.com>
2022-04-14 14:46:04 -0400

    div by zero error resolution

--- a1z26.py
+++ a1z26.py
@@ -2,7 +2,7 @@ from random import *
 import time
 enum_list=list(enumerate(list("abcdefghijklmnopqrstuvwxyz"),1))
 enum_list.extend([(l[1],l[0]) for l in enum_list])
-qs_answered, correct, time_limit = 0,0,15
+qs_answered, correct, time_limit = 0,0,5
 start_time=time.time()
 incorrect_list=[]
 while time.time()-start_time<time_limit:
@@ -14,10 +14,13 @@ while time.time()-start_time<time_limit:
     qs_answered+=1
     if input()==str(c[1]): correct+=1
     else: incorrect_list.append(c)
+    time_diff=time.time()-start_time
 print("\n"*20)
 print(f'After {time_limit} seconds, your average answer-per-second is {round(time_diff/qs_answered,3)}.')
+if correct:
+    print(f'Your average correct-answer-per-second is {round(time_diff/correct,3)}.')
 print(f'You answered {correct}/{qs_answered} questions correctly.')
 if incorrect_list:
-    print("Here is a list of your missed words: ")
+    print("Here are your incorrect answers: ")
     for k,v in incorrect_list:
-        print(str(k) +", "+str(v))
+        print(str(k) +" -> "+str(v))
