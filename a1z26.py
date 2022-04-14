2e5cc1585f840897f5288b9124bb1ca072ccec08
Corey Wang <corefire2004@gmail.com>
2022-04-14 14:35:52 -0400

    added incorrect_list

--- a1z26.py
+++ a1z26.py
@@ -2,8 +2,9 @@ from random import *
 import time
 enum_list=list(enumerate(list("abcdefghijklmnopqrstuvwxyz"),1))
 enum_list.extend([(l[1],l[0]) for l in enum_list])
-qs_answered, correct, time_limit = 0,0,30
+qs_answered, correct, time_limit = 0,0,15
 start_time=time.time()
+incorrect_list=[]
 while time.time()-start_time<time_limit:
     c=choice(enum_list)
     print("\n"*20)
@@ -12,6 +13,11 @@ while time.time()-start_time<time_limit:
     print(f'You have {round(time_limit-time_diff,3)} seconds left')
     qs_answered+=1
     if input()==str(c[1]): correct+=1
-    
-print(f'After {time_limit} seconds, your average answers per second is ' 
-    f'{round(time_diff/qs_answered,3)}, and you answered {correct}/{qs_answered} questions correctly.')
+    else: incorrect_list.append(c)
+print("\n"*20)
+print(f'After {time_limit} seconds, your average answer-per-second is {round(time_diff/qs_answered,3)}.')
+print(f'You answered {correct}/{qs_answered} questions correctly.')
+if incorrect_list:
+    print("Here is a list of your missed words: ")
+    for k,v in incorrect_list:
+        print(str(k) +", "+str(v))
