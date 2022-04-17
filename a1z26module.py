3a5f49a15b44b9e7528b20ebd2434b4be3f4788d
Corey Wang <corefire2004@gmail.com>
2022-04-16 21:07:12 -0400

    take input

--- a1z26module.py
+++ a1z26module.py
@@ -1,6 +1,6 @@
 from random import *
 d=dict(list(enumerate(list("abcdefghijklmnopqrstuvwxyz"),1))+[(l[1],l[0]) for l in list(enumerate(list("abcdefghijklmnopqrstuvwxyz"),1))])
-i="corey wang"
+i=input("Enter string to encode with a1z26: \n")
 s=""
 for c in i:
     if c in d:
