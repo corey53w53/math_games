0cf8d059c7d6d20bda6667744b03327b29653ebf
Corey Wang <corefire2004@gmail.com>
2022-04-16 21:31:48 -0400

    separate dicts

--- a1z26module.py
+++ a1z26module.py
@@ -1,14 +1,14 @@
 from random import *
-d=dict(list(enumerate(list("abcdefghijklmnopqrstuvwxyz"),1))+[(l[1],l[0]) for l in list(enumerate(list("abcdefghijklmnopqrstuvwxyz"),1))])
-
+letter_to_num=dict(list(enumerate(list("abcdefghijklmnopqrstuvwxyz"),1)))
+num_to_letter=dict([(l[1],l[0]) for l in list(enumerate(list("abcdefghijklmnopqrstuvwxyz"),1))])
+d=letter_to_num+num_to_letter
 def convert_a1z26(i):
     s=""
     for c in i:
-        if c in d: s+=str(d[c])
+        if c in letter_to_num: s+=str(d[c])
         else: s+=c
         s+=" "
     return s
-
 i=input("Enter string to encode with a1z26: \n")
 s=convert_a1z26(i)
 print(s)
