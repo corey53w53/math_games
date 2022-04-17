5de1da5fef5bcbf98dae16c1b02f4fb7066d5257
Corey Wang <corefire2004@gmail.com>
2022-04-16 21:10:24 -0400

    made func

--- a1z26module.py
+++ a1z26module.py
@@ -1,9 +1,14 @@
 from random import *
 d=dict(list(enumerate(list("abcdefghijklmnopqrstuvwxyz"),1))+[(l[1],l[0]) for l in list(enumerate(list("abcdefghijklmnopqrstuvwxyz"),1))])
+
+def convert_a1z26(i):
+    s=""
+    for c in i:
+        if c in d: s+=str(d[c])
+        else: s+=c
+        s+=" "
+    return s
+
 i=input("Enter string to encode with a1z26: \n")
-s=""
-for c in i:
-    if c in d: s+=str(d[c])
-    else: s+=c
-    s+=" "
+s=convert_a1z26(i)
 print(s)
