707131f885af33450ef5e1acbd841af7004ead04
Corey Wang <corefire2004@gmail.com>
2022-04-16 21:39:17 -0400

    temp

--- a1z26module.py
+++ a1z26module.py
@@ -1,14 +1,22 @@
 from random import *
 letter_to_num=dict(list(enumerate(list("abcdefghijklmnopqrstuvwxyz"),1)))
-num_to_letter=dict([(l[1],l[0]) for l in list(enumerate(list("abcdefghijklmnopqrstuvwxyz"),1))])
+num_to_letter=dict([(str(l[1]),l[0]) for l in list(enumerate(list("abcdefghijklmnopqrstuvwxyz"),1))])
 d=letter_to_num+num_to_letter
-def convert_a1z26(i):
+def convert_to_a1z26(i):
     s=""
     for c in i:
         if c in letter_to_num: s+=str(d[c])
         else: s+=c
         s+=" "
     return s
+def convert_to_num(i):
+    s=""
+    for c in i.split():
+        if c in letter_to_num: s+=str(d[c])
+        else: s+=c
+        s+=" "
+    return s
 i=input("Enter string to encode with a1z26: \n")
-s=convert_a1z26(i)
+i="12 13 14 7"
+s=convert_to_num(i)
 print(s)
